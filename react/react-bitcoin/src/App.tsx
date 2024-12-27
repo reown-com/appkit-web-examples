23e6dbe71d1aaf1ec87c9394cbc8de2c63adbaee
Tomas Rawski <rtomas@gmail.com>
2024-12-27 09:03:52 -0300

    react bitcoin example

---
+++ react/react-bitcoin/src/App.tsx
@@ -0,0 +1,41 @@
+import { createAppKit } from '@reown/appkit/react'
+import { networks, projectId, metadata, bitcoinAdapter } from './config'
+import { ActionButtonList } from './components/ActionButtonList'
+import { InfoList } from './components/InfoList'
+
+import "./App.css"
+
+// Create a AppKit instance
+createAppKit({
+  adapters: [bitcoinAdapter],
+  networks,
+  metadata,
+  projectId,
+  themeMode: 'light',
+  features: {
+    analytics: true, // Optional - defaults to your Cloud configuration
+    socials: [],
+    email: false
+  }
+})
+
+export function App() {
+
+  return (
+    <div className={"pages"}>
+      <img src="/reown.svg" alt="Reown" style={{ width: '150px', height: '150px' }} />
+      <h1>AppKit bitcoin React dApp Example</h1>
+          <appkit-button />
+          <ActionButtonList />
+          <div className="advice">
+            <p>
+              This projectId only works on localhost. <br/>
+              Go to <a href="https://cloud.reown.com" target="_blank" className="link-button" rel="Reown Cloud">Reown Cloud</a> to get your own.
+            </p>
+          </div>
+          <InfoList />
+    </div>
+  )
+}
+
+export default App
