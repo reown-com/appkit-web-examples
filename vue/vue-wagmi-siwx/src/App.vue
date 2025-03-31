1ded9b3daaeb305c47d442b9b9f77dbda332b32c
Tomas R <rtomas@gmail.com>
2025-03-31 13:52:31 -0300

    add vue wagmi siwx example

---
+++ vue/vue-wagmi-siwx/src/App.vue
@@ -0,0 +1,59 @@
+
+<template>
+   <div class="pages">
+      <img src="/reown.svg" alt="Reown" width="150" height="150" />
+      <h1>AppKit wagmi vue Example</h1>
+
+      <appkit-button />
+      <ActionButtonList />
+      <div className="advice">
+        <p>
+          This projectId only works on localhost. <br/>
+          Go to <a href="https://cloud.reown.com" target="_blank" className="link-button" rel="Reown Cloud">Reown Cloud</a> to get your own.
+        </p>
+      </div>
+      <InfoList />
+    </div>
+</template>
+
+
+<script>
+import {
+  createAppKit,
+} from '@reown/appkit/vue'
+import { DefaultSIWX } from "@reown/appkit-siwx";
+import {wagmiAdapter , networks, projectId } from './config/index'
+
+import ActionButtonList from "./components/ActionButton.vue"; 
+import InfoList from "./components/InfoList.vue";
+
+// Initialize AppKit
+const appkit = createAppKit({
+  adapters: [wagmiAdapter],
+  networks,
+  projectId,
+  themeMode: 'light',
+  features: {
+    connectMethodsOrder: ['email', 'social', 'wallet'],
+    analytics: true, // Optional - defaults to your Cloud configuration
+  },
+  siwx: new DefaultSIWX(), // only add this line to use SIWX in appkit
+  metadata: {
+    name: 'AppKit Vue Example',
+    description: 'AppKit Vue Example',
+    url: 'https://reown.com/appkit',
+    icons: ['https://avatars.githubusercontent.com/u/179229932?s=200&v=4']
+  },
+  themeVariables: {
+    '--w3m-accent': '#000000',
+  }
+})
+
+export default {
+  name: "App",
+  components: {
+    ActionButtonList,
+    InfoList
+  },
+};
+</script>
