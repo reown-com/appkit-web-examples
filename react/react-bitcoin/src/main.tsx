70759f5a255c68a240f1c499a6933bef6b915d9c
TomTom <rtomas@gmail.com>
2025-04-03 16:34:08 -0300

    fix buffer and ethereum error

--- react/react-bitcoin/src/main.tsx
+++ react/react-bitcoin/src/main.tsx
@@ -1,6 +1,10 @@
+import { Buffer } from 'buffer'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+
+// Make Buffer available globally
+window.Buffer = Buffer
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
