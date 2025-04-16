c3aefaf7e17f20b80c6a0d7c15f5a7a217127a01
TomTom <rtomas@gmail.com>
2025-04-16 00:36:39 -0300

    add config file

--- react/react-wagmi-smart-session/server/server.js
+++ react/react-wagmi-smart-session/server/server.js
@@ -4,39 +4,11 @@ import { isAddress, encodeFunctionData, toHex, parseEther } from "viem";
 //import { SmartSessionGrantPermissionsResponse } from "@reown/appkit-experimental/smart-session";
 import { privateKeyToAccount , signMessage } from "viem/accounts";
 import { prepareCalls, handleFetchReceipt, sendPreparedCalls } from "./util/prepareCalls.js";
+import { storageABI } from "./config/index.ts";
 
 // get env variables
 import dotenv from 'dotenv';
 dotenv.config();
-
-const storageABI = [
-	{
-		"inputs": [],
-		"name": "retrieve",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "num",
-				"type": "uint256"
-			}
-		],
-		"name": "store",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	}
-]
 
 const app = express();
 
