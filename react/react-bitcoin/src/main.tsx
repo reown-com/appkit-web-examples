35cdd2ed82e0c5b6d513d51d534948e571d03059
TomTom <rtomas@gmail.com>
2025-04-16 13:10:01 -0300

    improve bitcoin example

--- react/react-bitcoin/src/main.tsx
+++ react/react-bitcoin/src/main.tsx
@@ -1,10 +1,8 @@
-import { Buffer } from 'buffer'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 
-// Make Buffer available globally
-window.Buffer = Buffer
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
