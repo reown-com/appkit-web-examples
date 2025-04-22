745fa4e8bfe1c236e368eb327ae396a94a4944a8
TomTom <rtomas@gmail.com>
2025-04-23 01:44:09 +0300

    trying to sign a msg

--- vue/vue-core/src/components/ActionButton.vue
+++ vue/vue-core/src/components/ActionButton.vue
@@ -3,11 +3,12 @@
     <button @click="openAppKit">Open</button>
     <button @click="handleDisconnect">Disconnect</button>
     <button @click="switchToNetwork">Switch</button>
+    <button @click="handleSignMsg">Sign MSG</button>
   </div>
 </template>
 
 <script>
-import { useDisconnect, useAppKit, useAppKitNetwork } from "@reown/appkit/vue";
+import { useDisconnect, useAppKit, useAppKitNetwork, useAppKitAccount, useAppKitProvider } from "@reown/appkit/vue";
 import { networks } from "../config/index";
 
 export default {
@@ -16,6 +17,9 @@ export default {
     const { disconnect } = useDisconnect();
     const { open } = useAppKit();
     const networkData = useAppKitNetwork();
+    const { address } = useAppKitAccount() // AppKit hook to get the address and check if the user is connected
+    const { walletProvider}  = useAppKitProvider('eip155');
+    console.log(">> walletProvider", walletProvider);
 
     const openAppKit = () => open();
     const switchToNetwork = () => networkData.value.switchNetwork(networks[1]);
@@ -28,10 +32,29 @@ export default {
     };
 
 
+    // function to sing a msg 
+    const handleSignMsg = async () => {
+      const message = "Hello Reown AppKit!" // message to sign
+      try {
+        const result = await walletProvider.request({
+          method: 'personal_sign',
+          params: [message, address]
+        }) 
+        
+        console.log("result", result);
+      } catch (error) {
+        console.log("error", error);
+        throw new Error(error);
+      }
+    }
+
+
+
     return {
       handleDisconnect,
       openAppKit,
       switchToNetwork,
+      handleSignMsg
     };
   },
 };
