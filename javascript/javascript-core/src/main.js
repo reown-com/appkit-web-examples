ef7e8e568c491f18bf3ee771269aaa7900186db9
TomTom <rtomas@gmail.com>
2025-04-28 17:14:01 -0300

    Appkit Core with Wallet Button

--- javascript/javascript-core/src/main.js
+++ javascript/javascript-core/src/main.js
@@ -3,8 +3,12 @@ import { store } from './store/appkitStore'
 import { updateTheme, updateButtonVisibility } from './utils/dom'
 import { initializeSubscribers } from './utils/suscribers'
 import { signMessage } from './services/wallet'
+import { createAppKitWalletButton } from '@reown/appkit-wallet-button'
 // Initialize subscribers
-initializeSubscribers(appKit)
+initializeSubscribers(appKit);
+
+// Create AppKit Wallet Button
+const appKitWalletButton = createAppKitWalletButton();
 
 // Initial check
 updateButtonVisibility(appKit.getIsConnectedState());
@@ -26,6 +30,24 @@ document.getElementById('sign-message')?.addEventListener(
 
     document.getElementById('signatureState').innerHTML = signature
     document.getElementById('signatureSection').style.display = ''
+  }
+)
+
+document.getElementById('walletButton')?.addEventListener(
+  'click', async () => {
+    // check if it's ready
+    if (appKitWalletButton.isReady()) {
+      appKitWalletButton
+          .connect('walletConnect')
+          .then(data => {
+              console.log('connected', data)
+          })
+          .catch(err => {
+              console.log('error connecting', err)
+          })
+    } else {
+      console.log('appKitWalletButton is not ready')
+    }
   }
 )
 
