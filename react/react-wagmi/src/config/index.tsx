5a38a6159ccaf402db13b2cf20e6fa869b35c8e1
TomTom <rtomas@gmail.com>
2025-05-11 17:43:23 -0300

    custom connectors config

--- react/react-wagmi/src/config/index.tsx
+++ react/react-wagmi/src/config/index.tsx
@@ -1,7 +1,7 @@
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 import { mainnet, arbitrum, sepolia } from '@reown/appkit/networks'
 import type { AppKitNetwork } from '@reown/appkit/networks'
-
+import { walletConnect, coinbaseWallet, injected } from 'wagmi/connectors'
 // Get projectId from https://cloud.reown.com
 export const projectId = import.meta.env.VITE_PROJECT_ID || "b56e18d47c72ab683b10814fe9495694" // this is a public projectId only to use on localhost
 
@@ -19,10 +19,22 @@ export const metadata = {
 // for custom networks visit -> https://docs.reown.com/appkit/react/core/custom-networks
 export const networks = [mainnet, arbitrum, sepolia] as [AppKitNetwork, ...AppKitNetwork[]]
 
+const connectors: CreateConnectorFn[] = []
+connectors.push(walletConnect({ projectId, metadata, showQrModal: false })) // showQrModal must be false
+connectors.push(injected({ shimDisconnect: true }))
+connectors.push(
+  coinbaseWallet({
+    appName: metadata.name,
+    appLogoUrl: metadata.icons[0]
+  })
+)
+
 //Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
+  connectors,
   projectId,
   networks
 })
+
 
 export const config = wagmiAdapter.wagmiConfig
