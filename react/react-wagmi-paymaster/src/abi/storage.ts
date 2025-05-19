0404dc8a08d150e85f17730e9ea81a70d4ed2970
TomTom <rtomas@gmail.com>
2025-05-19 19:04:08 -0300

    first version of the paymaster example

---
+++ react/react-wagmi-paymaster/src/abi/storage.ts
@@ -0,0 +1,30 @@
+export const storageABI = [
+	{
+		"inputs": [],
+		"name": "retrieve",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "num",
+				"type": "uint256"
+			}
+		],
+		"name": "store",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
+
+export const storageAddress = "0xEe6D291CC60d7CeD6627fA4cd8506912245c8cA4"
---
+++ react/react-wagmi-paymaster/src/components/PaymasterActionButtonList.tsx
@@ -0,0 +1,72 @@
+import { useAppKitNetwork, useAppKitAccount  } from '@reown/appkit/react'
+import { useReadContract, useSendCalls } from 'wagmi'
+import { storageABI, storageAddress } from '../abi/storage'
+import { parseEther, encodeFunctionData } from 'viem'
+
+const randomNumber = Math.floor(Math.random() * 999) + 1;
+
+const storeCallData = encodeFunctionData({
+  abi: storageABI,
+  functionName: 'store',
+  args: [randomNumber]
+})
+
+const STORAGE_TEST_TX = {
+  to: storageAddress as `0x${string}`,
+  value: parseEther('0'),
+  data: storeCallData
+}
+
+
+export const PaymasterActionButtonList = () => {
+    const { isConnected } = useAppKitAccount() // AppKit hook to get the address and check if the user is connected
+    const { chainId } = useAppKitNetwork()
+    const { sendCalls } = useSendCalls({
+        mutation: {
+          onSuccess: hash => {
+            console.log("SendCalls Success", hash)
+          },
+          onError: (error: any ) => {
+            console.log("SendCalls Error", error)
+          }
+        }
+      });
+    const readContract = useReadContract({
+      address: storageAddress,
+      abi: storageABI,
+      functionName: 'retrieve',
+      query: {
+        enabled: false, // disable the query in onload
+      }
+    })
+
+    const handleReadSmartContract = async () => {
+      console.log("Read Sepolia Smart Contract");
+      const { data } = await readContract.refetch();
+      console.log("data: ", data)
+    }
+
+    const handleWriteSmartContract = () => {
+        console.log("Write Sepolia GASLESS Smart Contract")
+        sendCalls({
+          calls: [STORAGE_TEST_TX],
+          chainId: 11155111,
+          capabilities: {
+            paymasterService: {
+              url: "https://paymaster-api.reown.com/11155111/rpc?projectId=07fe00193f4e4938dd3bee5142803995", // for sepolia 
+            }
+          }
+        })
+    }
+
+
+
+  return (
+    isConnected && chainId === 11155111 && ( // Only show the buttons if the user is connected to Sepolia
+    <div>
+        <button onClick={handleReadSmartContract}>Read Sepolia Smart Contract</button>
+        <button onClick={handleWriteSmartContract}>Write Sepolia Gasless Smart Contract</button>  
+    </div>
+    )
+  )
+}
