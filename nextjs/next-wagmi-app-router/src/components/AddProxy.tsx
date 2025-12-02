7d164cfb8e05336a46b6eb608a2f5d1565ee9a94
MK <mago.khamidov@gmail.com>
2025-12-02 13:11:36 +0000

    chore: quick fix

--- nextjs/next-wagmi-app-router/src/components/AddProxy.tsx
+++ nextjs/next-wagmi-app-router/src/components/AddProxy.tsx
@@ -1,172 +1,174 @@
-'use client';
+"use client";
 
-import { useAppKitAccount } from '@reown/appkit/react';
-import type { JSX } from 'react';
-import { useWriteContract } from 'wagmi';
-import { defineChain } from 'viem';
+import { useAppKitAccount } from "@reown/appkit/react";
+import type { JSX } from "react";
+import { useWriteContract } from "wagmi";
+import { defineChain } from "viem";
+import { dataHavenTestnet } from "@/config";
 
 export const ABI = [
-    {
-      inputs: [
-        {
-          internalType: 'address',
-          name: 'delegate',
-          type: 'address',
-        },
-        {
-          internalType: 'enum Proxy.ProxyType',
-          name: 'proxyType',
-          type: 'uint8',
-        },
-        {
-          internalType: 'uint32',
-          name: 'delay',
-          type: 'uint32',
-        },
-      ],
-      name: 'addProxy',
-      outputs: [],
-      stateMutability: 'nonpayable',
-      type: 'function',
-    },
-    {
-      inputs: [
-        {
-          internalType: 'address',
-          name: 'real',
-          type: 'address',
-        },
-        {
-          internalType: 'address',
-          name: 'delegate',
-          type: 'address',
-        },
-        {
-          internalType: 'enum Proxy.ProxyType',
-          name: 'proxyType',
-          type: 'uint8',
-        },
-        {
-          internalType: 'uint32',
-          name: 'delay',
-          type: 'uint32',
-        },
-      ],
-      name: 'isProxy',
-      outputs: [
-        {
-          internalType: 'bool',
-          name: 'exists',
-          type: 'bool',
-        },
-      ],
-      stateMutability: 'view',
-      type: 'function',
-    },
-    {
-      inputs: [
-        {
-          internalType: 'address',
-          name: 'real',
-          type: 'address',
-        },
-        {
-          internalType: 'address',
-          name: 'callTo',
-          type: 'address',
-        },
-        {
-          internalType: 'bytes',
-          name: 'callData',
-          type: 'bytes',
-        },
-      ],
-      name: 'proxy',
-      outputs: [],
-      stateMutability: 'payable',
-      type: 'function',
-    },
-    {
-      inputs: [
-        {
-          internalType: 'address',
-          name: 'real',
-          type: 'address',
-        },
-        {
-          internalType: 'enum Proxy.ProxyType',
-          name: 'forceProxyType',
-          type: 'uint8',
-        },
-        {
-          internalType: 'address',
-          name: 'callTo',
-          type: 'address',
-        },
-        {
-          internalType: 'bytes',
-          name: 'callData',
-          type: 'bytes',
-        },
-      ],
-      name: 'proxyForceType',
-      outputs: [],
-      stateMutability: 'payable',
-      type: 'function',
-    },
-    {
-      inputs: [],
-      name: 'removeProxies',
-      outputs: [],
-      stateMutability: 'nonpayable',
-      type: 'function',
-    },
-    {
-      inputs: [
-        {
-          internalType: 'address',
-          name: 'delegate',
-          type: 'address',
-        },
-        {
-          internalType: 'enum Proxy.ProxyType',
-          name: 'proxyType',
-          type: 'uint8',
-        },
-        {
-          internalType: 'uint32',
-          name: 'delay',
-          type: 'uint32',
-        },
-      ],
-      name: 'removeProxy',
-      outputs: [],
-      stateMutability: 'nonpayable',
-      type: 'function',
-    },
-  ] as const;
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "delegate",
+        type: "address",
+      },
+      {
+        internalType: "enum Proxy.ProxyType",
+        name: "proxyType",
+        type: "uint8",
+      },
+      {
+        internalType: "uint32",
+        name: "delay",
+        type: "uint32",
+      },
+    ],
+    name: "addProxy",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "real",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "delegate",
+        type: "address",
+      },
+      {
+        internalType: "enum Proxy.ProxyType",
+        name: "proxyType",
+        type: "uint8",
+      },
+      {
+        internalType: "uint32",
+        name: "delay",
+        type: "uint32",
+      },
+    ],
+    name: "isProxy",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "exists",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "real",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "callTo",
+        type: "address",
+      },
+      {
+        internalType: "bytes",
+        name: "callData",
+        type: "bytes",
+      },
+    ],
+    name: "proxy",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "real",
+        type: "address",
+      },
+      {
+        internalType: "enum Proxy.ProxyType",
+        name: "forceProxyType",
+        type: "uint8",
+      },
+      {
+        internalType: "address",
+        name: "callTo",
+        type: "address",
+      },
+      {
+        internalType: "bytes",
+        name: "callData",
+        type: "bytes",
+      },
+    ],
+    name: "proxyForceType",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "removeProxies",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "delegate",
+        type: "address",
+      },
+      {
+        internalType: "enum Proxy.ProxyType",
+        name: "proxyType",
+        type: "uint8",
+      },
+      {
+        internalType: "uint32",
+        name: "delay",
+        type: "uint32",
+      },
+    ],
+    name: "removeProxy",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+] as const;
 
 export const PROXY_CONTRACT_ADDRESS =
-  '0x000000000000000000000000000000000000080b';
-
+  "0x000000000000000000000000000000000000080b";
 
 export const AddProxy = (): JSX.Element => {
   const { address, isConnected, status } = useAppKitAccount();
 
-  console.count('--------------------------------');
-  console.log('address', address);
-  console.log('isConnected', isConnected);
-  console.log('status', status);
-  console.log('--------------------------------');
+  console.count("--------------------------------");
+  console.log("address", address);
+  console.log("isConnected", isConnected);
+  console.log("status", status);
+  console.log("--------------------------------");
 
   const { writeContract, data, isPending, isError, error } = useWriteContract();
 
   const addProxy = async () => {
     writeContract({
+      chain: dataHavenTestnet,
       address: PROXY_CONTRACT_ADDRESS,
       abi: ABI,
-      functionName: 'addProxy',
-      args: ['0xd693b7ed1f6d6e93983fdc3135f6b2ba771078ac', 0, 0],
+      functionName: "addProxy",
+      args: ["0xd693b7ed1f6d6e93983fdc3135f6b2ba771078ac", 0, 0],
+      gas: BigInt(1000000),
     });
   };
 
--- nextjs/next-wagmi-app-router/src/config/index.ts
+++ nextjs/next-wagmi-app-router/src/config/index.ts
@@ -1,50 +1,52 @@
-import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-import { defineChain } from '@reown/appkit/networks'
-import type { AppKitNetwork } from '@reown/appkit/networks'
-import { http } from 'viem';
+import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
+import { defineChain } from "@reown/appkit/networks";
+import type { AppKitNetwork } from "@reown/appkit/networks";
+import { http } from "viem";
 
-const DATAHAVEN_TESTNET_ID = 55931
-const DATAHAVEN_RPC_URL = 'https://services.datahaven-testnet.network/testnet'
+// Get projectId from https://dashboard.reown.com
+export const projectId =
+  process.env.NEXT_PUBLIC_PROJECT_ID || "b56e18d47c72ab683b10814fe9495694"; // this is a public projectId only to use on localhost
 
-export const dataHavenTestnet: AppKitNetwork = defineChain({
-  id: DATAHAVEN_TESTNET_ID,
-  name: "DataHaven Testnet",
-  chainNamespace: 'eip155',
-  caipNetworkId: `eip155:55931`,
-  testnet: true,
+export const dataHavenTestnet = defineChain({
+  id: 55931,
+  name: "Datahaven Testnet",
+  chainNamespace: "eip155",
+  caipNetworkId: "eip155:55931",
   nativeCurrency: {
+    decimals: 18,
     name: "MOCK",
     symbol: "MOCK",
-    decimals: 18,
   },
   rpcUrls: {
-    [DATAHAVEN_TESTNET_ID]: { http: [DATAHAVEN_RPC_URL] },
-    default: { http: [DATAHAVEN_RPC_URL] },
-    public: { http: [DATAHAVEN_RPC_URL] },
+    default: {
+      http: [
+        `https://rpc.walletconnect.org/v1/?chainId=eip155:55931&projectId=${projectId}`,
+      ],
+    },
   },
   blockExplorers: {
     default: {
-      name: "DataHaven Testnet",
-      url: "https://polkadot.js.org/apps?rpc=wss://services.datahaven-testnet.network/testnet#/explorer/query/",
+      name: "DhScan",
+      url: "https://testnet.dhscan.io/",
     },
   },
+  contracts: {},
+  testnet: true,
 });
-// Get projectId from https://dashboard.reown.com
-export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || "b56e18d47c72ab683b10814fe9495694" // this is a public projectId only to use on localhost
 
 if (!projectId) {
-  throw new Error('Project ID is not defined')
+  throw new Error("Project ID is not defined");
 }
 
-export const networks = [dataHavenTestnet] as [AppKitNetwork, ...AppKitNetwork[]]
+export const networks = [dataHavenTestnet] as [
+  AppKitNetwork,
+  ...AppKitNetwork[]
+];
 
 //Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
   projectId,
   networks,
-  transports: {
-    [dataHavenTestnet.id]: http('https://services.datahaven-testnet.network/testnet'),
-  },
-})
+});
 
-export const config = wagmiAdapter.wagmiConfig
+export const config = wagmiAdapter.wagmiConfig;
