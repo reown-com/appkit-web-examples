e72f63bebfd3b1f53069fd31d6e0188c8f8ad5fb
TomTom <rtomas@gmail.com>
2025-12-19 13:36:27 -0300

    fix porto issue

--- nextjs/next-siwx-multichain-supabase-storage/next.config.ts
+++ nextjs/next-siwx-multichain-supabase-storage/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   webpack: config => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding', 'porto')
+    config.externals.push('pino-pretty', 'lokijs', 'encoding')
     return config
   }
 };
