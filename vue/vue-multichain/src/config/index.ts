bcbae7a9b4bde1747e6974afc054d117ab01b4b2
TomTom <rtomas@gmail.com>
2026-01-14 20:44:33 -0300

    WagmiPlugin is  initialize after createAppKit

--- vue/vue-multichain/src/config/index.ts
+++ vue/vue-multichain/src/config/index.ts
@@ -1,5 +1,6 @@
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 import { SolanaAdapter } from '@reown/appkit-adapter-solana'
+import { createAppKit } from '@reown/appkit/vue'
 import { mainnet, polygon, base, solana, solanaTestnet, solanaDevnet, type AppKitNetwork } from '@reown/appkit/networks'
 
 export const projectId = import.meta.env.VITE_PROJECT_ID || "b56e18d47c72ab683b10814fe9495694" // this is a public projectId only to use on localhost
@@ -15,3 +16,23 @@ export const wagmiAdapter = new WagmiAdapter({
 })
 
 export const solanaWeb3JsAdapter = new SolanaAdapter()
+
+// createAppKit must be called before WagmiPlugin is registered
+createAppKit({
+  adapters: [wagmiAdapter, solanaWeb3JsAdapter],
+  networks,
+  projectId,
+  themeMode: 'light',
+  features: {
+    analytics: true
+  },
+  metadata: {
+    name: 'AppKit Vue Example',
+    description: 'AppKit Vue Example',
+    url: 'https://reown.com/appkit',
+    icons: ['https://avatars.githubusercontent.com/u/179229932?s=200&v=4']
+  },
+  themeVariables: {
+    '--w3m-accent': '#000000',
+  }
+})
