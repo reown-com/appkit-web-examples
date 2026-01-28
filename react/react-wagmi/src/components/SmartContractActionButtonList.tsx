0dec8239440c8e67118e057124ac188ac6b1eb48
TomTom <rtomas@gmail.com>
2026-01-28 18:06:27 -0300

    test SC in dataheaven

--- react/react-wagmi/src/components/SmartContractActionButtonList.tsx
+++ react/react-wagmi/src/components/SmartContractActionButtonList.tsx
@@ -2,82 +2,68 @@
 // if you are not going to read or write smart contract, you can delete this file
 //
 
-import { useAppKitNetwork, useAppKitAccount  } from '@reown/appkit/react'
-import { useReadContract, useWriteContract } from 'wagmi'
+import { useAppKitNetwork, useAppKitAccount } from '@reown/appkit/react'
+import { useWriteContract } from 'wagmi'
 import { useEffect } from 'react'
-const storageABI = [
-	{
-		"inputs": [],
-		"name": "retrieve",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "num",
-				"type": "uint256"
-			}
-		],
-		"name": "store",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	}
+
+const createBucketABI = [
+  {
+    inputs: [
+      { name: 'mspId', type: 'bytes32' },
+      { name: 'name', type: 'bytes' },
+      { name: '_private', type: 'bool' },
+      { name: 'valuePropId', type: 'bytes32' }
+    ],
+    name: 'createBucket',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function'
+  }
 ]
 
-const storageSC = "0xEe6D291CC60d7CeD6627fA4cd8506912245c8cA4" 
+const contractAddress = '0x0000000000000000000000000000000000000404'
+const DATAHAVEN_TESTNET_CHAIN_ID = 55931
 
 export const SmartContractActionButtonList = () => {
-    const { isConnected } = useAppKitAccount() // AppKit hook to get the address and check if the user is connected
-    const { chainId } = useAppKitNetwork()
-    const { writeContract, isSuccess } = useWriteContract()
-    const readContract = useReadContract({
-      address: storageSC,
-      abi: storageABI,
-      functionName: 'retrieve',
-      query: {
-        enabled: false, // disable the query in onload
-      }
+  const { isConnected } = useAppKitAccount()
+  const { chainId } = useAppKitNetwork()
+  const { writeContract, isSuccess, isPending, error } = useWriteContract()
+
+  useEffect(() => {
+    if (isSuccess) {
+      console.log('createBucket contract write success')
+    }
+  }, [isSuccess])
+
+  useEffect(() => {
+    if (error) {
+      console.error('createBucket error:', error)
+    }
+  }, [error])
+
+  const handleCreateBucket = () => {
+    console.log('Calling createBucket on DataHaven Testnet')
+    writeContract({
+      address: contractAddress,
+      abi: createBucketABI,
+      functionName: 'createBucket',
+      args: [
+        '0x0000000000000000000000000000000000000000000000000000000000000001', // mspId
+        '0x746573742d736f6369616c2d776974682d72656f776e2d636861696e', // name ("test-social-with-reown-chain" in hex)
+        false, // _private
+        '0x628a23c7aa64902e13f63ffdd0725e07723745f84cabda048d901020d200da1e' // valuePropId
+      ]
     })
-
-    useEffect(() => {
-      if (isSuccess) {
-        console.log("contract write success");
-      }
-    }, [isSuccess])
-
-    const handleReadSmartContract = async () => {
-      console.log("Read Sepolia Smart Contract");
-      const { data } = await readContract.refetch();
-      console.log("data: ", data)
-    }
-
-    const handleWriteSmartContract = () => {
-        console.log("Write Sepolia Smart Contract")
-        writeContract({
-          address: storageSC,
-          abi: storageABI,
-          functionName: 'store',
-          args: [123n],
-        })
-    }
-
+  }
 
   return (
-    isConnected && chainId === 11155111 && ( // Only show the buttons if the user is connected to Sepolia
-    <div >
-        <button onClick={handleReadSmartContract}>Read Sepolia Smart Contract</button>
-        <button onClick={handleWriteSmartContract}>Write Sepolia Smart Contract</button>  
-    </div>
+    isConnected &&
+    chainId === DATAHAVEN_TESTNET_CHAIN_ID && (
+      <div>
+        <button onClick={handleCreateBucket} disabled={isPending}>
+          {isPending ? 'Creating Bucket...' : 'Create Bucket (DataHaven)'}
+        </button>
+      </div>
     )
   )
 }
--- react/react-wagmi/src/config/index.tsx
+++ react/react-wagmi/src/config/index.tsx
@@ -1,5 +1,5 @@
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-import { mainnet, arbitrum, sepolia } from '@reown/appkit/networks'
+import { datahavenTestnet } from '@reown/appkit/networks'
 import type { AppKitNetwork } from '@reown/appkit/networks'
 
 // Get projectId from https://dashboard.reown.com
@@ -17,7 +17,7 @@ export const metadata = {
   }
 
 // for custom networks visit -> https://docs.reown.com/appkit/react/core/custom-networks
-export const networks = [mainnet, arbitrum, sepolia] as [AppKitNetwork, ...AppKitNetwork[]]
+export const networks = [datahavenTestnet] as [AppKitNetwork, ...AppKitNetwork[]]
 
 //Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
