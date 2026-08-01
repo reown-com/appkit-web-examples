72efb39e84caec029e94f3c8a4ad105a884ef16e
MK <mago.khamidov@gmail.com>
2026-08-01 18:10:31 +0100

    fix: add x402 externals

--- nextjs/next-multichain-app-router/next.config.ts
+++ nextjs/next-multichain-app-router/next.config.ts
@@ -2,7 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   webpack: config => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding', 'porto')
+    config.externals.push(
+      'pino-pretty',
+      'lokijs',
+      'encoding',
+      'porto',
+      '@x402/evm',
+      '@x402/evm/upto/client',
+      '@x402/evm/exact/client',
+      '@x402/core/client',
+      '@x402/svm/exact/client'
+    )
     return config
   }
 };
